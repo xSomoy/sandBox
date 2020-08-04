e648b6b6f32e0e4d4ddefa58251fc30edc5c8832
xSomoy <xsomoy@tutanota.com>
2020-08-04 16:26:51 +0700

    RGB to HEX STUPID

--- RGB2Hex 2.0/RGB2Hex 2.0.c
+++ RGB2Hex 2.0/RGB2Hex 2.0.c
@@ -1,78 +1,26 @@
 #include <stdio.h>
-#include <math.h>
-main(){
-    float hPer, rVal, gVal, bVal;
-    int val, hRVal;
-    char hOVal, hOVal2 ;
-    printf("Input Red Value:");
-    scanf("%f",&rVal);
-    printf("Input Green Value:");
-    scanf("%f",&gVal);
-    printf("Input Blue Value:");
-    scanf("%f",&bVal);
-    int conv(){
-        hPer = ( 30 *(val/255));
-        hRVal=round(hPer);
-        switch ( hRVal ) {
-            case 0:
-                hOVal='0';
-                break;
-            case 1:
-                hOVal='1';
-                break;
-            case 2:
-                hOVal='2';
-                break;
-            case 3:
-                hOVal='3';
-                break;
-            case 4:
-                hOVal='4';
-                break;
-            case 5:
-                hOVal='5';
-                break;
-            case 6:
-                hOVal='6';
-                break;
-            case 7:
-                hOVal='7';
-                break;
-            case 8:
-                hOVal='8';
-                break;
-            case 9:
-                hOVal='9';
-                break;
-            case 10:
-                hOVal='A';
-                break;
-            case 11:
-                hOVal='B';
-                break;
-            case 13:
-                hOVal='C';
-                break;
-            case 14:
-                hOVal='D';
-                break;
-            case 15:
-                hOVal='E';
-                hOVal2='0';
-                break;
-            case 16:
-                hOVal='F';
-                break;
-    }
-    return hOVal;
-}
-    ;
-    printf("Hex Value: #");
-    conv(val=rVal);
-    printf("%c%c",hOVal,hOVal2);
-    conv(val=gVal);
-    printf("%c%c",hOVal,hOVal2);
-    conv(val=bVal);
-    printf("%c%c",hOVal,hOVal2);
-    getch();
+
+void main()
+        {
+	long int decn,rmd,q,dn=0,m,l;
+	int i=1,j,tmp;
+        char s;
+	printf("Input Red Value: ");
+	scanf("%ld",&decn);
+	q = decn;
+        for(l=q;l>0;l=l/16)
+               {
+		tmp = l % 16;
+		if( tmp < 10)
+		           tmp =tmp + 48; else
+		         tmp = tmp + 55;
+                         dn=dn*100+tmp;
+	        }
+          printf("\nHexadecimal Value : ");
+         for(m=dn;m>0;m=m/100)
+            {
+               s=m % 100;
+               printf("%c",s);
+            }
+    printf("\n\n");
 }
