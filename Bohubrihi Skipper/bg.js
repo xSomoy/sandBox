78a2dddccca9a3b1523aa5f3fed866671a7bda9a
xSomoy <xsomoy@tutanota.com>
2020-09-02 23:54:51 +0700

    Extension URl changed

--- Bohubrihi Skipper/bg.js
+++ Bohubrihi Skipper/bg.js
@@ -1,6 +1,6 @@
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_Tab_Info => {
-        if(/^https:\/\/www\.google/.test(current_Tab_Info.url)) {
+        if(/^https:\/\/www\.bohubrihi/.test(current_Tab_Info.url)) {
             chrome.tabs.executeScript(null, {file:'./skip.js'}, ()=> console.log('i injected'))
         }
 
