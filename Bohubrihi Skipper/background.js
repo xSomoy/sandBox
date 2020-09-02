1f210f7027b85806c6cc0b5ae3f03dba6eab1cd0
xSomoy <xsomoy@tutanota.com>
2020-09-03 02:40:13 +0700

    working on button apprance

--- Bohubrihi Skipper/background.js
+++ Bohubrihi Skipper/background.js
@@ -1,9 +1,9 @@
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_Tab_Info => {
         if(/^https:\/\/www\.bohubrihi/.test(current_Tab_Info.url)) {
-            chrome.tabs.executeScript(null, {file:'./forground.js'}, ()=> console.log('i injected'))
+            chrome.tabs.insertCSS(null,{file:'./style.css'});
+            chrome.tabs.executeScript(null, {file:'./forground.js'});
         }
 
     });
 });
-//console.log('test');
--- Bohubrihi Skipper/forground.js
+++ Bohubrihi Skipper/forground.js
@@ -1,1 +1,10 @@
-document.getElementsByClassName("sfwd-mark-complete")[0].submit();
+var r = document.getElementById("ld-content-actions").childElementCount;
+console.log(r)
+
+//if (r == 'disabled') {
+//    const skipButton = document.createElement('button');
+//    skipButton.innerText = "Skip This Lesson";
+//    skipButton.id = "skipButton";
+//    document.querySelector('body').appendChild(skipButton);
+//}
+//document.getElementsByClassName("sfwd-mark-complete")[0].submit();
