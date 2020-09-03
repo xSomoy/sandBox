0b070c94e411e2cc453b2fd967c8e66ba6a47b1a
xSomoy <xsomoy@tutanota.com>
2020-09-04 02:44:11 +0700

    continious content check added

--- Bohubrihi Skipper/background.js
+++ Bohubrihi Skipper/background.js
@@ -1,9 +1,17 @@
 chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_Tab_Info => {
-        if(/^https:\/\/www\.bohubrihi/.test(current_Tab_Info.url)) {
-            chrome.tabs.insertCSS(null,{file:'./style.css'});
-            chrome.tabs.executeScript(null, {file:'./forground.js'});
-        }
+    function check() {
+        chrome.tabs.get(tab.tabId, current_Tab_Info => {
+            console.log(current_Tab_Info);
+            if (/^https:\/\/www\.bohubrihi/.test(current_Tab_Info.url)) {
+                chrome.tabs.insertCSS(null, {
+                    file: './style.css'
+                });
+                chrome.tabs.executeScript(null, {
+                    file: './forground.js'
+                });
+            }
 
-    });
+        });
+    };
+    setInterval(check,1000);
 });
--- Bohubrihi Skipper/forground.js
+++ Bohubrihi Skipper/forground.js
@@ -1,8 +1,16 @@
-var element = document.getElementsByClassName('sfwd-mark-complete').length;
-if (element == 2) {
+function check() {
+    var element = document.getElementsByClassName('sfwd-mark-complete').length;
+    if (element == 2) {
         const skipButton = document.createElement('button');
         skipButton.innerText = "Skip This Lesson";
         skipButton.id = "skipButton";
         document.querySelector('body').appendChild(skipButton);
-}
-//document.getElementsByClassName("sfwd-mark-complete")[0].submit();
+        skipButton.addEventListener("click", skip);
+        console.log('aloha');
+    }
+};
+setInterval(check, 500);
+
+function skip() {
+    document.getElementsByClassName("sfwd-mark-complete")[0].submit();
+};
