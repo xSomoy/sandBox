e25ffa08541df897c988a133264cda7bfc869a94
xSomoy <xsomoy@tutanota.com>
2020-09-04 03:58:07 +0700

    unpacked version done

--- Bohubrihi Skipper/script/background.js
+++ Bohubrihi Skipper/script/background.js
@@ -3,10 +3,10 @@ chrome.tabs.onActivated.addListener(tab => {
         chrome.tabs.get(tab.tabId, current_Tab_Info => {
             if (/^https:\/\/www\.bohubrihi/.test(current_Tab_Info.url)) {
                 chrome.tabs.insertCSS(null, {
-                    file: './style.css'
+                    file: './css/style.css'
                 });
                 chrome.tabs.executeScript(null, {
-                    file: './forground.js'
+                    file: './script/forground.js'
                 });
             }
 
