206329f92ff73ba282e3d40a8d962158f91cd99e
xSomoy <xsomoy@tutanota.com>
2020-09-04 02:54:47 +0700

    almost ready

--- Bohubrihi Skipper/background.js
+++ Bohubrihi Skipper/background.js
@@ -1,7 +1,6 @@
 chrome.tabs.onActivated.addListener(tab => {
     function check() {
         chrome.tabs.get(tab.tabId, current_Tab_Info => {
-            console.log(current_Tab_Info);
             if (/^https:\/\/www\.bohubrihi/.test(current_Tab_Info.url)) {
                 chrome.tabs.insertCSS(null, {
                     file: './style.css'
