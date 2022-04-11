637029ebfef76255eb001d379ac13ba0621c7486
jDeadman <xsomoy@tutanota.com>
2022-04-11 19:37:55 +0600

    timeCalc v1.0.1

--- timeCalc/timeCalc.py
+++ timeCalc/timeCalc.py
@@ -1,11 +1,11 @@
 # this program display information about time 
-# Version 1.0.0
+# Version 1.0.1
 import datetime , os , time
 x = datetime.datetime.now()
 hour = int(x.strftime("%H")) * 60
 minute = int(x.strftime("%M"))
 def timer(): 
-    print('Year: ', x.strftime('%Y'))
+    print('Year:', x.strftime('%Y'))
     print('Day: ', x.strftime('%j'))
     print( 'Minutes Passed:', hour + minute)
     print('Minute Remains:', 1440 - ( hour + minute))
