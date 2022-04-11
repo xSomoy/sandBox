96e7329bf33eb74c317b1eee9ba513519f770721
jDeadman <xsomoy@tutanota.com>
2022-04-11 18:52:58 +0600

    timeCalc python

--- timeCalc.py
+++ timeCalc.py
@@ -1,13 +1,15 @@
-import datetime
-import time
+import datetime , os , time
 x = datetime.datetime.now()
 hour = int(x.strftime("%H")) * 60
 minute = int(x.strftime("%M"))
 def timer(): 
-    print(x)
+    print('Day: ', x.strftime('%Y'))
+    print('Day: ', x.strftime('%j'))
     print( 'Minutes Passed:', hour + minute)
     print('Minute Remains:', 1440 - ( hour + minute))
 
 while True:
     timer()
+    time.sleep(10)
+    os.system('cls')
     
