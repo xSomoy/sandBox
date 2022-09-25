cd0259d0f28342c0c513beb9ac093a3acb71d9f0
jDeadman <xsomoy@tutanota.com>
2022-09-25 19:00:24 +0600

    Interest Calc

--- Interest Calc/intCalc.py
+++ Interest Calc/intCalc.py
@@ -1,8 +1,6 @@
 # Rules 30k interest on 100k loan for 12months
 # find out the interest rate now
 
-from multiprocessing import current_process
-
 
 rate = 0.025
 loanAmount = float(input("how much did you loan?: "))
@@ -10,3 +8,4 @@ months = float(input("how many months have passed?: "))
 currentInterest = rate * loanAmount * months
 totalDue = loanAmount + currentInterest
 print("Your Current Interest is: " + str(currentInterest) + " BDT" )
+print("Total Due: " + str(totalDue) + " BDT")
