5df0f5d2c20120d5f8cc6f8ee003a953312b6bcd
John Deadman <xwsomoy@tutanota.com>
2022-10-27 18:49:54 +0600

    bkash cashout calculator

--- bKash Calculater/bKashCal.py
+++ bKash Calculater/bKashCal.py
@@ -1,7 +1,14 @@
-a = input("How much you want to cash out?: ")
+a = int(input("How much you want to cash out?: "))
+
+def agentCat():
+    agent= input("Priyo Agent? : ")
+    if agent == 'yes':
+        return 1.49
+    else:
+        return 1.85
+
 def cashOutCalc(b):
-    b = int(b)
-    commisionRate = 1.85
+    commisionRate = agentCat()
     totalCommision = (b * commisionRate) / 100
     returnCash = b - totalCommision
     print('Return Cash: ' +  str(returnCash))
