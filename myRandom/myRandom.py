640f54e85e72d6144e13561b80a96fde65dff68f
John Deadman <xwsomoy@tutanota.com>
2022-11-10 01:11:21 +0600

    test

---
+++ myRandom/myRandom.py
@@ -0,0 +1,19 @@
+# importing datetime module for now()
+import datetime
+ 
+# using now() to get current time
+current_time = datetime.datetime.now()
+y = int(input("Enter Random Number Lenght: "))
+a = int(input('Input Random Seed: ')) + current_time.hour
+b = int(current_time.minute) + a
+c = int(current_time.month) + b
+d = int(current_time.year) + c
+
+x = int((((a+b+c+d) - (a-b-c-d) * (a*b*c*d) / (a/b/c/d)) + 1 ) / 16)
+while True:
+    if len(str(x)) > y:
+        x = x / len(str(x))
+        print(x)
+    else:
+        break
+print(x)
