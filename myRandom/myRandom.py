be3f9423cefdfef3ab87da530e96bf6ce9b5358e
John Deadman <xwsomoy@tutanota.com>
2022-11-10 11:23:53 +0600

    my randomNumber Generator

--- myRandom/myRandom.py
+++ myRandom/myRandom.py
@@ -4,14 +4,13 @@ import datetime
 # using now() to get current time
 current_time = datetime.datetime.now()
 y = int(input("Enter Random Number Lenght: "))
-a = int(input('Input Random Seed: ')) + current_time.hour
-b = int(current_time.minute) + a
-c = int(current_time.month) + b
-d = int(current_time.year) + c
+a = int(input('Input Random Seed: ')) + current_time.microsecond
+b = int(current_time.second) + a
+c = int(current_time.minute) + b
+d = int(current_time.hour) + c
 
 x = int((((a+b+c+d) - (a-b-c-d) * (a*b*c*d) / (a/b/c/d)) + 1 ) / 16)
 
 while len(str(x)) > y:
-    x = x / len(str(x))
-    print(x)
+    x = int(x / len(str(x)))
 print(x)
