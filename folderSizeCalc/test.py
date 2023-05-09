0992aa62ef7eee23cee8f0d1d793e84edc5bc40c
John Deadman <xSomoy@protonmail.com>
2023-05-10 04:34:42 +0600

    working on v3

--- folderSizeCalc/test.py
+++ folderSizeCalc/test.py
@@ -1,3 +1,21 @@
 import datetime
-d = datetime.date()
-print(d)
+
+now = datetime.datetime.now()
+
+print("Current date and time:")
+print(now)
+
+print('\n\n\n')
+# Display only the date
+print("Current date:", now.date())
+
+# Display only the time
+print("Current time:", now.time())
+
+# Display the date and time in a custom format
+print("Formatted date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
+
+print('\n\n\n')
+
+print(now.date())
+print(str(now.time()))
