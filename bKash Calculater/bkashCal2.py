fc790a0624ff946952cd76a50faeec435c135527
John Deadman <47719599+xSomoy@users.noreply.github.com>
2024-04-24 12:49:24 +0600

    Update bkashCal2.py
    doing the math

--- bKash Calculater/bkashCal2.py
+++ bKash Calculater/bkashCal2.py
@@ -1,6 +1,15 @@
+#bKash Calc V2.0
+#Mushohyqur Rahman Tanveer
+Tanveer
+
+#Taking input
 a = int(input('Amount: '))
 
+# Charge in %
 pAgent = 1.49
 agent = 1.85
 atm = 1.49
 bank = 1.25
+
+copa = (a * 1000) / pAgent
+print(copa)
