dd81622516791f20892de88ebfc55f7a07e915c7
John Deadman <47719599+xSomoy@users.noreply.github.com>
2024-06-11 12:31:11 +0600

    bKash Calc v2.1 done

--- bKash Calculater/bKashCalc_v2.1.py
+++ bKash Calculater/bKashCalc_v2.1.py
@@ -12,12 +12,16 @@ bank = 1.25
 
 copa = (a * pAgent) / 100
 print('Prio Agent: ' + str(copa))
+print('=> Hard Cash:' + str(a - copa))
 
 coa = (a * agent) / 100
 print('Agent: ' + str(coa))
+print('=> Hard Cash:' + str(a - coa))
 
 coatm = (a * atm) / 100
 print('ATM: ' + str(coatm))
+print('=> Hard Cash:' + str(a - coatm))
 
 cobank = (a * bank) / 100
 print('Bank: ' + str(cobank))
+print('=> Hard Cash:' + str(a - cobank))
