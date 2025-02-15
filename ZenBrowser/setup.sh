385b3584df4effffd5155bc9940c020792442ba4
TENET <xSomoy@protonmail.com>
2025-02-15 22:06:32 +0600

    zen installer done

--- ZenBrowser/setup.sh
+++ ZenBrowser/setup.sh
@@ -1,3 +1,6 @@
 curl https://updates.zen-browser.app/appimage.sh > appimage.sh
+sudo chmod +x appimage.sh
 ./appimage.sh
+echo "/home/tenet/.local/share/AppImage/ZenBrowser.AppImage" > zen
+sudo chmod +x zen
 sudo cp zen /usr/bin/
