bd879441f1c74500e436f70ddebbe092da739533
TENET <xSomoy@protonmail.com>
2025-03-18 15:28:27 +0600

    pizegami for ghostty

--- ZenBrowser/setup.sh
+++
@@ -1,6 +0,0 @@
-curl https://updates.zen-browser.app/appimage.sh > appimage.sh
-sudo chmod +x appimage.sh
-./appimage.sh
-echo "/home/tenet/.local/share/AppImage/ZenBrowser.AppImage" > zen
-sudo chmod +x zen
-sudo cp zen /usr/bin/
